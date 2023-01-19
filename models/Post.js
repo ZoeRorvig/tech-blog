49660895631ca5f26a3f40e7f014eb178aedb83d
Zoe Rorvig <zrorvig@gmail.com>
2023-01-18 22:22:17 -0500

    updated Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,7 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+class Post extends Model {}
 
 Post.init(
     {
